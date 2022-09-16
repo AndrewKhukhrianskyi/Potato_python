6ba734de03e64d35540b599caedf31944a43cece
unknown <kennyman143@gmail.com>
2022-09-16 18:28:52 +0300

    adds hw.py 10.09

---
+++ 10.09/hw.py
@@ -0,0 +1,22 @@
+# Task 1 - Year
+print('Task 1 - Year')
+year = int(input('Enter year: '))
+print('Your age is:', 2021 - year)
+
+# Task 2 - Perimeter
+print('Task 2 - Perimeter')
+amount_of_lines = int(input('Amount of lines: '))
+line_length = int(input('Line length: '))
+print('Perimeter: ', amount_of_lines * line_length)
+
+# Task 3 - Runner
+print('Task 3 - Runner')
+water = 20
+water_per_circle = 0.5
+print('Circles: ', water * water_per_circle)
+
+# Task 4 - Runner (Mod)
+print('Task 3 - Runner Mod')
+water = int(input('Amount of water: '))
+water_per_circle = float(input('Water per circle: '))
+print('Circles: ', water * water_per_circle)
