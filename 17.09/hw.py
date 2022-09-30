72011d4ee8ae79759a8e8c24e4b31835c3a68f2a
unknown <kennyman143@gmail.com>
2022-09-30 13:36:51 +0300

    adds hw + cw 30.09

---
+++ 17.09/hw.py
@@ -0,0 +1,13 @@
+# Task 1 - Hashtag Generator
+hashtag_value = input('Enter value: ')
+print('HASHTAG:', '#' + hashtag_value.title().replace(' ', ''))
+
+# Task 2 - Nya adder
+word = input('Enter text: ')
+print('Result: ', word + 'Nya!')
+
+# Task 3 - Cylinder
+radius = float(input('Radius: '))
+height = float(input('Height: '))
+print('Cylinder Perimeter:', 3.14 * 2 * radius * height)
+
