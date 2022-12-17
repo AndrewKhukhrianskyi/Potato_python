0a9fb3a6a8079eb0e5833dc928576815f975dd92
AndrewKhukhrianskyi <kennyman143@gmail.com>
2022-12-17 15:01:28 +0200

    adds project updates

--- TEAMS/team_a/game.py
+++ TEAMS/team_a/game.py
@@ -2,17 +2,19 @@ from random import randint
 
 def create_killer():
     def set_stat(abilities):
-        stat_list = {}
+        stat_list = {"Меткая: увеличивает шанс попадания" : 5,
+                     "Социальный хамелеон: подстраиваетесь под окружение и людей" : 5,
+                     "Плохое зрение: уменьшает шанс попадания, а также негативно влияет на некоторые действия в событиях": -5,
+                     "Социофоб: Тяжело общаться с людьми" : -5}
         set_positive_stat = False
         set_negative_stat = False
-        print(stat_list)
-        for skill_name, description in stat_list.items():
+        print(list(stat_list.keys()))
+        while True:
             try:
-                print(skill_name + ': ' + description)
-                pos_skill = input('Выберите положительную черту из списка выше: ')
+                pos_skill = input('Выберите положительную черту из списка выше: ').capitalize()
                 set_positive_stat = True
-                neg_skill = input('Выберите негативную черту из списка выше: ')
-                set_negative_stat = False
+                neg_skill = input('Выберите негативную черту из списка выше: ').capitalize()
+                set_negative_stat = True
                 if pos_skill and neg_skill:
                     abilities.append(stat_list[pos_skill])
                     abilities.append(stat_list[neg_skill])
@@ -48,4 +50,42 @@ def create_killer():
 
     return name, abilities
 
-create_killer()
+
+def get_perks(perk):
+    database_perks = {'Отравитель: Шанс отравления увеличен' : 10,
+                      "Штрафник: Шанс забить жертву в бою голыми руками увеличен" : 5,
+                      "Хитман: Получить все ачивки по убийствам" : 20,
+                      "Разорванная селезенка: Получить критическую неудачу при сопротивлении жертвы": -5,
+                      "Болевые точки: Критический шанс на убийство одним ударом увеличен" : 5,
+                      "Гладиатор: Шанс убийства оппонента в бою увеличивается в два раза" : 20,
+                      "Морда кирпичом: Шанс убеждения увеличен" : 5,
+                      "Неудачник: Вы никого не убили и вам почему-то не везет..": -50,
+                      "Призрак Спарты: Вы остаетесь в здравом психическом состоянии." : 0}
+    return database[perk]
+
+def fight(killer_hp, hero, enemy_count, enemy_hp, enemy_dmg, item_list):
+    crit_chance = 5
+    player_hit_chance = 50
+    while killer_hp > 0 and enemy_hp > 0:
+            p_chance = randint(1, 100)
+            c_chance = randint(1, 100)
+            if player_hit_chance >= p_chance:
+                if crit_chance >= c_chance:
+                    print('Жертва пала замертво...')
+                    return killer_hp, hero, item_list
+                enemy_hp[enemy] -= player_dmg
+            else:
+                print("промах")
+    if killer_hp < 0:
+        return 'Game Over'
+    elif enemy_hp < 0:
+        print("После долгой борьбы жертва была убита...")
+        return killer_hp, hero, item_list
+
+# TODO - наисать главу 1 в виде функции
+    
+hero = create_killer()
+hp = 100
+psychical_mood = 0
+items = []
+
--- TEAMS/team_b/game.py
+++ TEAMS/team_b/game.py
@@ -2,31 +2,44 @@ from random import randint
 
 def create_character():
     def add_abilities(abilities):
-        database_abilities = {"Ловкач" : ("Шанс уклонения увеличен", -5)
-                              "Крепкий орешек" : ("Способность выдержать удар без потери здоровья.", -6)
-                              "Атлет" : ("Шанс побега увеличен", -4)
-                              "Богач" : ("Навык убеждения и подкупа увеличен", -4) 
+        database_abilities = {"Ловкач" : ("Шанс уклонения увеличен", -5),
+                              "Крепкий орешек" : ("Способность выдержать удар без потери здоровья.", -6),
+                              "Атлет" : ("Шанс побега увеличен", -4),
+                              "Богач" : ("Навык убеждения и подкупа увеличен", -4) ,
                               "Острый слух" : ("Навык восприятия и обезвреживания ловушек увеличен", -2),
                               "Смертник" : ("Вы ничего не боитесь и легко переносите урон, но можно очень быстро стать калекой.",
                                             0),
                               "Жирдяй" : ("Шанс побега уменьшен, также вы быстро устаете", 4),
                               "Забывчивость" : ("Навык восприятия и обезвреживания ловушек уменьшен. Есть шанс наткнуться на ту же ловушку.", 2),
                               "Пацифист" : ("Урон снижен в два раза", 10),
-                              "Хронический простатит" : ("Все параметры снижены пятикратно.", 0),
+                              "Хронический простатит" : ("Все параметры снижены пятикратно.", 10),
                               "Астматик" : ("Вы не можете сбежать с поля боя.", 8)}
         
             
-
+        # TODO - Удалять выбранный навык из базы
             
         number = 0
         while True:
             set_char = input('Добавляем характеристику? Да\Нет: ').lower()
+            if set_char == 'да':
+                print(database_abilities)
+                set_ability = input("Введите навык:").capitalize()
+                if set_ability in database_abilities:
+                    abilities.append(set_ability)
+                    number += database_abilities[set_ability][1]
+                    database_abilities.pop(set_ability)
+            elif set_char == 'нет':
+                if number >= 0:
+                    return abilities
+                else:
+                    print(number)
+                    print("Выберите отрицательные навыки, чтобы сбалансировать персонажа.")
             
     name = 'Radga'
     set_name = input('Меняем имя? Да\Нет: ').lower()
     if set_name == "да":
         name = input('Введите новое имя вашего индуса: ')
-    elif set_name == "Нет":
+    elif set_name == "нет":
         print("Установлено имя по умолчанию: Radga")
     else:
         print("Error. Неизвестная команда.")
@@ -34,7 +47,7 @@ def create_character():
     set_abilities = input('Устанавливаем характеристики? Да\Нет: ').lower()
     abilities = []
     if set_abilities == "да":
-        pass
+        add_abilities(abilities)
     elif set_abilities == 'нет':
         print("Пустые характеристики.")
         return name
@@ -43,4 +56,39 @@ def create_character():
 
     return name, abilities
 
-    
+database_items = {'Название' : ["Повторяемое название", "Урон"],
+                  'Нож' : ["Нож", 10],
+                  }
+hero = create_character()
+
+stamina = 100
+hp = 100
+items = []
+print(hero)
+
+def fight_enemy(player, player_dmg, enemy_list):
+    player_chance = 50
+    enemy_name = []
+    enemy_dmg = []
+    enemy_chance = []
+    enemy_hp = []
+    for enemy_data in enemy_list:
+        enemy_name.append(enemy_data[0]) # Name
+        enemy_dmg.append(enemy_data[1]) # dmg
+        enemy_chance.append(enemy_data[2]) # chance
+        enemy_hp.append(enemy_data[3]) # hp
+
+    while True:
+        for enemy in range(len(enemy_list)):
+            p_chance = randint(1, 100)
+            if player_chance >= p_chance:
+                if enemy_hp[enemy] <= 0:
+                    pass
+                else:
+                    print("Удар по врагу!")
+                    enemy_hp[enemy] -= player_dmg
+            else:
+                print("промах")
+    # TODO - Написать функцию с главами 1-2
+                
+                
