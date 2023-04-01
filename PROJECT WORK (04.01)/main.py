1990c74cf1e7ed98e3aad948d07e0027ac0a4fc8
AndrewKhukhrianskyi <kennyman143@gmail.com>
2023-04-01 14:51:49 +0300

    adds improvements

--- PROJECT WORK (04.01)/main.py
+++ PROJECT WORK (04.01)/main.py
@@ -7,13 +7,25 @@ from config import *
 from tkinter import *
 
 import tkinter.messagebox as mb
+import re
+
+def find_text():
+    regex_pattern = regex_text_field.get(0.0, END).strip()
+    text = text_field.get(0.0, END).strip()
+
+    result = re.findall(rf'{regex_pattern}', text)
+
+    result_text_field.delete(0.0, END)
+    result_text_field.insert(0.0, str(result))
+
+    mb.showinfo(title='Результат',
+                message = "Данные найдены!")
 
 def clear_data():
     ask_message = mb.askyesno(title='Вопрос',
                               message = "Уверены, что хотите очистить данные?")
     if ask_message:
-        widgets = [regex_text_field,
-                   text_field,
+        widgets = [text_field,
                    result_text_field]
         for widget in widgets:
             widget.delete(0.0, END)
@@ -33,25 +45,36 @@ window.title(TITLE)
 
 regex_label = Label(width = LABEL_WIDTH,
                     text = 'Напишите регулярное выражение ниже:')
+
 regex_text_field = Text(width = TEXT_ENTRY_WIDTH,
                         height = TEXT_ENTRY_HEIGHT)
 
 label_text_field = Label(width = LABEL_WIDTH,
                           text = 'Напишите текст снизу:')
+
 text_field = Text(width = TEXT_WIDTH,
                   height = TEXT_HEIGHT)
+
 result_label = Label(width = LABEL_WIDTH,
                      text = 'Результат:')
+
 result_text_field = Text(width = TEXT_WIDTH,
                          height = TEXT_HEIGHT + 10)
+
 clear_button = Button(text = 'Очистить!',
                       width = BUTTON_WIDTH,
                       height = BUTTON_HEIGHT,
                       command = clear_data)
+
+find_button = Button (text = 'Найти!',
+                      width = BUTTON_WIDTH,
+                      height = BUTTON_HEIGHT,
+                      command = find_text)
+
 widgets = [regex_label, regex_text_field,
            label_text_field, text_field,
            result_label, result_text_field,
-           clear_button]
+           clear_button, find_button]
 
 for widget in widgets:
     widget.pack(anchor = 'n')
